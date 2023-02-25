d1b004ee5335d31814b2340d1cc45b975073a948
d4rrrk <58345897+d4rrrk@users.noreply.github.com>
2023-02-25 16:55:11 +0000

    Update creation date.lua

--- creation date.lua
+++ creation date.lua
@@ -1,4 +1,4 @@
-print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+--// made by D4rk
 local players = game:GetService("Players"):GetChildren()
 local months = {
 	"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
@@ -8,5 +8,4 @@ for i, v in pairs(players) do
     local secondsExisted = v.accountAge * 86400
     local dateObject = os.date("!*t", os.time() - secondsExisted)
     print(v.Name.." joined ROBLOX on "..months[dateObject.month].." "..dateObject.day..", "..dateObject.year.." and their user ID is "..v.UserId)
-    print(" ")
 end
