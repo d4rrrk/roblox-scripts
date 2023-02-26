d0ba96064b1feca78d87f34c5bcd2279fac96840
d4rrrk <58345897+d4rrrk@users.noreply.github.com>
2023-02-26 23:54:54 +0000

    Update fakewokenChestFarm.lua

--- fakewokenChestFarm.lua
+++ fakewokenChestFarm.lua
@@ -1,3 +1,21 @@
+--[[
+DDDDDDDDDDDDD               444444444                     kkkkkkkk           
+D::::::::::::DDD           4::::::::4                     k::::::k           
+D:::::::::::::::DD        4:::::::::4                     k::::::k           
+DDD:::::DDDDD:::::D      4::::44::::4                     k::::::k           
+  D:::::D    D:::::D    4::::4 4::::4  rrrrr   rrrrrrrrr   k:::::k    kkkkkkk
+  D:::::D     D:::::D  4::::4  4::::4  r::::rrr:::::::::r  k:::::k   k:::::k 
+  D:::::D     D:::::D 4::::4   4::::4  r:::::::::::::::::r k:::::k  k:::::k  
+  D:::::D     D:::::D4::::444444::::444rr::::::rrrrr::::::rk:::::k k:::::k   
+  D:::::D     D:::::D4::::::::::::::::4 r:::::r     r:::::rk::::::k:::::k    
+  D:::::D     D:::::D4444444444:::::444 r:::::r     rrrrrrrk:::::::::::k     
+  D:::::D     D:::::D          4::::4   r:::::r            k:::::::::::k     
+  D:::::D    D:::::D           4::::4   r:::::r            k::::::k:::::k    
+DDD:::::DDDDD:::::D            4::::4   r:::::r           k::::::k k:::::k   
+D:::::::::::::::DD           44::::::44 r:::::r           k::::::k  k:::::k  
+D::::::::::::DDD             4::::::::4 r:::::r           k::::::k   k:::::k 
+DDDDDDDDDDDDD                4444444444 rrrrrrr           kkkkkkkk    kkkkkkk
+]]--
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 if game.PlaceId == 10138901829 then
