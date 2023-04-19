003f85c66ef3e2be2673ad6776be742c73af5cf4
d4rrrk <58345897+d4rrrk@users.noreply.github.com>
2023-04-19 21:23:27 +0100

    Update RogueLineageDays.lua

--- RogueLineageDays.lua
+++ RogueLineageDays.lua
@@ -1,4 +1,4 @@
-print("hello")
+repeat wait(0.1) until game:IsLoaded()
 
 game:GetService("Players").LocalPlayer.PlayerGui.StartMenu.Finish:FireServer()
 
@@ -55,6 +55,4 @@ local checkLoop = run.RenderStepped:Connect(function()
     end
 end)
 
-game:GetService("Players").LocalPlayer.OnTeleport:Connect(function()
-    syn.queue_on_teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/RogueLineageDays.lua"))()]])
-end)
+syn.queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/RogueLineageDays.lua"))
