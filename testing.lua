c2f1498c85657f61ccfebfe6558e6c9c108c6244
d4rrrk <58345897+d4rrrk@users.noreply.github.com>
2023-04-19 21:15:27 +0100

    Create testing.lua

---
+++ testing.lua
@@ -0,0 +1,7 @@
+print("this script was executed on rejoining")
+
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        syn.queue_on_teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/testing.lua"))()]])
+    end
+end)
