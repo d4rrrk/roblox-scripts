196b6c2f871b8a7b812df051bc3227cc0f98ade5
d4rrrk <58345897+d4rrrk@users.noreply.github.com>
2023-04-19 21:18:47 +0100

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -3,5 +3,5 @@ repeat wait(1) until game:IsLoaded()
 print("this script was executed on rejoining")
 
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function()
-    syn.queue_on_teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/testing.lua"))()]])
+    syn.queue_on_teleport([[game:HttpGet("https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/testing.lua")]])
 end)
