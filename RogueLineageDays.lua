e5bb9c0e03480ab8631007678770067ba1f36abf
d4rrrk <58345897+d4rrrk@users.noreply.github.com>
2023-04-19 23:48:41 +0100

    Update RogueLineageDays.lua

--- RogueLineageDays.lua
+++ RogueLineageDays.lua
@@ -1,21 +1,6 @@
 repeat wait(1) until game:IsLoaded()
 
-syn.queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/RogueLineageDays.lua"))
-
 game:GetService("Players").LocalPlayer.PlayerGui.StartMenu.Finish:FireServer()
-
-repeat wait(1) until game:GetService("Players").LocalPlayer:FindFirstChild("Character")
-
-local function druidCheck()
-    for _, v in pairs(players:GetPlayers()) do
-        if v.Character then
-            if v.Backpack:FindFirstChild("Perflora") or v.Character:FindFirstChild("Perflora") then
-                hop("Druid "..(v.Name).." in server...")
-                break
-            end
-        end
-    end
-end
 
 local function hop(msg)
     game:GetService("StarterGui"):SetCore("PromptBlockPlayer", game.Players:GetPlayers()[math.random(2,#game.Players:GetPlayers())])
@@ -28,25 +13,37 @@ local function hop(msg)
     game:GetService("TeleportService"):Teleport(3016661674, game.Players.LocalPlayer)
 end
 
+local function druidCheck()
+    for _, v in pairs(game:GetService("Players"):GetChildren()) do
+        if v.Character and v.Name ~= game:GetService("Players").LocalPlayer then
+            if v.Backpack:FindFirstChild("Perflora") or v.Character:FindFirstChild("Perflora") then
+                hop("Druid "..(v.Name).." in server...")
+            end
+        end
+    end
+end
+
 game:GetService("Players").LocalPlayer.Idled:connect(function()
     game:GetService("VirtualUser"):CaptureController()
     game:GetService("VirtualUser"):ClickButton2(Vector2.new())
 end)
 
-local loop = game:GetService("RunService").RenderStepped:Connect(function()
-    local players = game:GetService("Players")
+syn.queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/RogueLineageDays.lua"))
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    druidCheck()
+    local live = game:GetService("Workspace").Live
     local player = players.LocalPlayer
     local root = player.Character.HumanoidRootPart
     
-    for _, v in pairs(players:GetChildren()) do
+    for _, v in pairs(live:GetChildren()) do
         if string.sub(v.Name, 1, 1) ~= "." and v.Name ~= game.Players.LocalPlayer.Name then
             if v.Character ~= nil then
                 local rootPos = root.Position
-                local otherPos = v.Character.HumanoidRootPart.Position
+                local otherPos = v.HumanoidRootPart.Position
                 local mag = math.round((rootPos - otherPos).Magnitude)
                 if mag <= 1000 then
                     hop("Player ("..v.Name..") too close.")
-                    loop:Disconnect()
                 end
             end
         end
