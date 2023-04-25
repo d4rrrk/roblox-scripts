711f96c3a9837a6dbb611a9f82da70e6b0faa8db
d4rrrk <58345897+d4rrrk@users.noreply.github.com>
2023-04-25 04:01:27 +0100

    Update RogueLineageDays.lua

--- RogueLineageDays.lua
+++ RogueLineageDays.lua
@@ -1,14 +1,14 @@
+repeat wait(1) until game:IsLoaded()
+repeat wait(0.1) until game:GetService("Players").LocalPlayer:FindFirstChild("PlayerGui")
+
 if game.PlaceId == 3016661674 then 
     syn.queue_on_teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/RogueLineageDays.lua"))()]])
     return
 end
 
-repeat wait(0.1) until game:IsLoaded()
-repeat wait(0.1) until game:GetService("Players").LocalPlayer:FindFirstChild("PlayerGui")
-
 game:GetService("Players").LocalPlayer.PlayerGui.StartMenu.Finish:FireServer()
 
-repeat wait(0.1) until game:GetService("Players").LocalPlayer.Character
+wait(3)
 
 game:GetService("Players").LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(1479, -105, -5750))
 wait(1)
